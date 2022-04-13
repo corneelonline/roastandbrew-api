3c1fa744d292d93512e1e6b530718396b3aca4c7
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-13 15:25:17 +0200

    Setup authentication on the API side with Laravel Sanctum

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only('show');
+    }
+
     public function show()
     {
-
+        return response()->json( $request->user() );
     }
 }
