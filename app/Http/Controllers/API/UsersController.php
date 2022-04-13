50ddde58a321612622db8570a7dec70fec895b8e
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-13 14:16:07 +0200

    Add API endpoints for later use

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
+
+class UsersController extends Controller
+{
+    public function show()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+/**
+ * Authentication Routes
+ */
+Route::post('/register', 'Auth\AuthController@register');
+Route::post('/login', 'Auth\AuthController@login');
+Route::post('/logout', 'Auth\AuthController@logout');
